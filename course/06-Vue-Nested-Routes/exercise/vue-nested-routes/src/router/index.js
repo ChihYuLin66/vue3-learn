4b733cc98f735e7d605998c629d3f0c4390fdb5c
ChihYuLin <chihyulin66@gmail.com>
2023-01-11 23:12:01 +0800

    vue router nested route

--- course/06-Vue-Nested-Routes/exercise/vue-nested-routes/src/router/index.js
+++ course/06-Vue-Nested-Routes/exercise/vue-nested-routes/src/router/index.js
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Guide from "../views/About/Guide.vue";
+import Changelog from "../views/About/Changelog.vue";
+import GitHub from "../views/About/GitHub.vue";
+import Reference from "../views/About/Reference.vue";
+import index from "../views/About/index.vue";
+
 const routes = [
   {
     path: "/",
@@ -9,7 +16,34 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    component: () => import("../views/About.vue"),
+    component: () => About,
+    children: [
+      {
+        path: "/about/index",
+        name: "aboutindex",
+        component: index,
+      },
+      {
+        path: "/about/guide",
+        name: "aboutGuide",
+        component: Guide,
+      },
+      {
+        path: "/about/changelog",
+        name: "aboutChangelog",
+        component: Changelog,
+      },
+      {
+        path: "/about/gitHub",
+        name: "aboutGitHub",
+        component: GitHub,
+      },
+      {
+        path: "/about/reference",
+        name: "aboutReference",
+        component: Reference,
+      },
+    ],
   },
 ];
 
