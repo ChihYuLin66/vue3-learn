a037690dd3aadd02f0bdd981953ef3b9d8c73e6a
ChihYuLin <chihyulin66@gmail.com>
2023-01-11 22:39:23 +0800

    土砲 a link active

--- course/05-VUE-Router-Example/exercise/src/App.vue
+++ course/05-VUE-Router-Example/exercise/src/App.vue
@@ -1,18 +1,44 @@
 <script>
+import { ref, watch } from "vue";
+import { useRoute } from "vue-router";
 export default {
   setup() {
-    return {};
+    // 土砲 a link active
+    const allRoutes = ["", "about", "courses"];
+    const route = useRoute();
+    const activeKey = ref(0);
+
+    watch(
+      () => route.path,
+      () => {
+        allRoutes.forEach((routeNmae, routeIndex) => {
+          let activeName = route.path.substr(1).split("/")[0];
+          if (activeName === routeNmae) {
+            activeKey.value = routeIndex;
+          }
+        });
+      }
+    );
+    return {
+      activeKey,
+    };
   },
 };
 </script>
 
 <template>
   <div id="nav">
-    <router-link to="/"> Home </router-link>
+    <router-link :class="{ active: activeKey === 0 }" to="/">
+      Home
+    </router-link>
     |
-    <router-link to="/about"> About </router-link>
+    <router-link :class="{ active: activeKey === 1 }" to="/about">
+      About
+    </router-link>
     |
-    <router-link to="/courses"> Courses </router-link>
+    <router-link :class="{ active: activeKey === 2 }" to="/courses">
+      Courses
+    </router-link>
   </div>
   <router-view />
 </template>
@@ -47,5 +73,9 @@ body {
       color: #42b983;
     }
   }
+
+  // .router-link-active {
+  //   color: red;
+  // }
 }
 </style>
